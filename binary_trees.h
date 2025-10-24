d4f3f20c3cc67ef52105d6f361f5a032bd95ebdb
noura398 <0nourasubaie@gmail.com>
2025-10-24 15:22:46 +0000

    a

--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,6 @@ typedef struct binary_tree_s heap_t;
 
 /* Task 0 prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
-
